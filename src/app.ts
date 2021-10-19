d102400fab286a66d34297f99f3ba67a62f8cfcd
Carlos Alves <carlosalves369m@hotmail.com>
2021-10-18 21:29:12 -0300

    Create login route and callback route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
+import 'dotenv/config'
 import express from 'express'
 
 const app = express()
 
+// Github authorization
+app.get('/github', (req, res) => {
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
 
+// 
+app.get('/signin/callback', (req, res) => {
+    const { code } = req.query
+
+    return res.json(code)
+})
 
 const PORT = 8080
 
