d5691fa93336d1e6996e6227697d690a461af3ca
Carlos Alves <carlosalves369m@hotmail.com>
2021-10-20 12:43:01 -0300

    fix bug in middleware ensureAuthenticated

--- src/middleware/ensureAuthenticated.ts
+++ src/middleware/ensureAuthenticated.ts
@@ -1,31 +1,32 @@
-import { Request, Request, NextFunction } from 'express'
-import { verify } from 'jsonwebtoken'
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
 
 interface IPayload {
-    sub: string
+  sub: string,
 }
 
-export function ensureAuthenticated(req:Request, res:Response, next:NextFunction) {
-    const authToken = req.headers.authorization
+export function ensureAuthenticated(request: Request, response: Response, next: NextFunction){
+  const authToken = request.headers.authorization
 
-    if(!authToken) {
-        return res.status(401).json({
-            errorCode: "token.invalid"
-        })
-    }
+  if(!authToken){
+    return response.status(401).json({
+      errorCode: 'token.invalid',
+    })
+  }
 
-    const [, token] = authToken.split(" ")
+  const [, token] = authToken.split(" ")
 
-    try {
-        const { sub }= verify(token, process.env.JWT_SECRET) as IPayload
+  try {
+    const { sub } = verify(token, process.env.JWT_SECRET) as IPayload
+    
+    request.user_id = sub
 
-        req.user_id = sub
-
-        return next()
-
-    } catch (err) {
-        return res.status(401).json({
-            errorCode: "token.expired"
-        })
-    }
+    return next()
+    
+  } catch(err) {
+    return response.status(401).json({
+      errorCode: 'token.expired'
+    })
+  }
+  
 }
